d8e9ce18c031222e8f449354c7fc20fe9f406c37
Mugerwa Joseph <josekodek2000@gmail.com>
2019-04-13 13:07:56 +0300

    Express

---
+++ api/Views/auth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const auth = express.Router();
+const auth_controller = require('../Controllers/auth_controller')
+
+auth.use('/signup', auth_controller.signup_User)
+
+auth.use('/signin', auth_controller.signin_User)
+
+exports.module = auth
