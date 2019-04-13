dfe53bbd7cd5f97cb450034184a60bb44d64b17c
Mugerwa Joseph <josekodek2000@gmail.com>
2019-04-13 13:03:48 +0300

    Express

--- api/Controllers/auth_controller.js
+++ api/Controllers/auth_controller.js
@@ -51,3 +51,50 @@ exports.signup_User = (req, res, next)=>{
         })
     })
 }
+
+exports.signin_User = (req, res, next)=>{
+    User.find({Email: req.body.Email})
+    .exec()
+    .then((user_found)=>{
+        if(user_found.length<1)
+        {
+            res.status(401).json({
+                Error: "Invalid Email or Password"
+            })
+        }
+
+        else
+        {
+            bcrypt.compare(req.body.Password, user_found[0].Password, (err, response)=>{
+                if(err)
+                {
+                    res.status(401).json({
+                        Error: "Invalid Email or Password"
+                    })
+                }
+
+                else
+                {
+                    const token = jwt.sign({
+                        Email: req.body.Email
+                    },
+                    process.env.JWT_TOKEN,
+                    {
+                        expiresIn: "2h"
+                    }
+                    )
+
+                    res.status(200).json({
+                        Message: "You are have Successfully Logged In",
+                        Token: token 
+                    })
+                }
+            })
+        }
+    })
+    .catch((err)=>{
+        res.status(401).json({
+            Error: err
+        })
+    })
+}
