22da769bafbd37a728b502e42f2e611396eaf5d2
Mugerwa Joseph <josekodek2000@gmail.com>
2019-04-13 18:17:11 +0300

    square-authentication

--- app/main_app.js
+++ app/main_app.js
@@ -18,7 +18,7 @@ const swaggerDefinition = {
         description: 'Endpoints to test the user registration/login routes'
     },
     schemes: ['https'],
-    host: 'https://square-authentication.herokuapp.com',
+    host: 'square-authentication.herokuapp.com',
     basePath: '/'
 };
 
